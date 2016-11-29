dd58abe41805f2b715f964eeee02803fb231d824
“lukemccrae” <lukemccrae@gmail.com>
2016-11-28 20:03:31 -0700

    finished the map section

--- map/core.js
+++ map/core.js
@@ -1,22 +1,34 @@
-function multiplyBy10 (array) {
- // your code here
+function multiplyBy10(array) {
+    return array.map(function(num) {
+        return num * 10;
+    });
 };
 
-function shiftRight (array) {
- // your code here
+function shiftRight(array) {
+    var result = array;
+    result.splice(0, 0, result[result.length - 1]);
+    result.splice(result.length - 1, 1)
+    return result;
 };
 
-function onlyVowels (array) {
- // your code here
+function onlyVowels(array) {
+    return array.map((letter) => {
+        return letter.replace(/[qwrtypsdfghjklzxcvbnm]/ig, '')
+    });
+}
+
+function doubleMatrix(array) {
+    return array.map((arr) => {
+        return arr.map((matrix) => {
+            return matrix * 2;
+        })
+    })
 };
 
-function doubleMatrix (array) {
- // your code here
-};
 
 module.exports = {
-  multiplyBy10: multiplyBy10,
-  shiftRight: shiftRight,
-  onlyVowels: onlyVowels,
-  doubleMatrix: doubleMatrix
-};
+    multiplyBy10: multiplyBy10,
+    shiftRight: shiftRight,
+    onlyVowels: onlyVowels,
+    doubleMatrix: doubleMatrix
+};;
